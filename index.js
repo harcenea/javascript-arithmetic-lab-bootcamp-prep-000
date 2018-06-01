9c683fdbc15cb32c03549849b5c2744890c38c28
Hugh A <harcenea@gmail.com>
2018-06-01 15:02:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function inc(n) { return n+= 1 ; }
 
 function dec(n) { return n-= 1 ; }
 
-function makeInt(n) { parseInt(n, 10) ; }
+function makeInt(n) {return parseInt(n, 10) ; }
 
-function preserveDecimal(n) { parseFloat(n) ; }
+function preserveDecimal(n) {return parseFloat(n) ; }
